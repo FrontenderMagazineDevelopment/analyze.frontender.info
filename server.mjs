4f412c0a2771c6bf94773a2a30bcc55d80e446f6
Anton Nemtsev <thesilentimp@gmail.com>
2020-06-23 20:57:58 +0200

    disable parallel analysis

--- server.mjs
+++ server.mjs
@@ -21,75 +21,81 @@ let connection;
       console.log('msg: ');
       console.log(JSON.parse(msg.content.toString()));
       const event = JSON.parse(msg.content.toString());
-      if (event.name === "ARTICLE_LINKS") event.payload.forEach(async (link)=>{
-        const builder = new ArticleBuilder();
-        builder.skip.stages = [
-          'github:before',
-          'github',
-          'github:after',
-        ];
-        builder.skip.plugins = [
-          { name: 'codepenTransform' },
-          { name: 'codepenTransformIFrame' },
-          { name: 'createREADME' },
-          { name: 'downloadImages' },
-          { name: 'writeMarkdown' },
-          { name: 'TMPDir' },
-          { name: 'initGithub' },
-          { name: 'uploadToRepo' },
-          { name: 'createRepo' },
-          { name: 'createREADME' },
-          { name: 'createCard' },
-        ];
-        try {
-          const result = await builder.create(link);
-          const description = (
-            (result.schema && result.schema.description) || 
-            (result.openGraph && result.openGraph.ogDescription) || 
-            null
-          );
-          const author = (
-                result.mercury && 
-                result.mercury.length > 0 && 
-                result.mercury[0].author && 
-                result.mercury[0].author.replace(/[\r\n]+/gm, '').trim()
-              ) || 
-              (result.schema && result.schema.author);
-          const article = {
-            url: link,
-            domain: result.domain,
-            author: author && [author],
-            tags: result.tags,
-            title:
-              (result.schema && result.schema.title) ||
-              (result.mercury && result.mercury.length > 0 && result.mercury[0].title) ||
-              (result.openGraph && result.openGraph.ogTitle),
-            description: description && description.replace(/[\r\n]+/gm, '').trim(),
-            lang:
-              (result.openGraph && result.openGraph.ogLocale) || 
-              result.language && result.language.length>0 && result.language[0].isReliable && result.language[0].language || 
-              'en',
-            published:
-              (result.openGraph && result.openGraph.article && result.openGraph.article.published_time) ||
-              (result.mercury && result.mercury.length > 0 && result.mercury[0].date_published) ||
-              (result.openGraph && result.openGraph.article && result.openGraph.article.modified_time) ||
-              (result.schema && result.schema.modified),
-            updated:
-              (result.openGraph && result.openGraph.article && result.openGraph.article.modified_time) ||
-              (result.mercury && result.mercury.length > 0 && result.mercury[0].date_published) ||
-              (result.schema && result.schema.modified),
-          };
-          console.log(article);
-          const msg = JSON.stringify({
-            name: "CREATE_ARTICLE",
-            payload: article,
-          });
-          await channel.sendToQueue(queue, Buffer.from(msg));
-          console.log(" [x] Sent %s", msg);
-        } catch (error) {
-          console.log(error);
+
+      if (event.name === "ARTICLE_LINKS") {
+        let index = event.payload.length;
+        while(index--) {
+          const link = event.payload[index];
+          const builder = new ArticleBuilder();
+          builder.skip.stages = [
+            'github:before',
+            'github',
+            'github:after',
+          ];
+          builder.skip.plugins = [
+            { name: 'codepenTransform' },
+            { name: 'codepenTransformIFrame' },
+            { name: 'createREADME' },
+            { name: 'downloadImages' },
+            { name: 'writeMarkdown' },
+            { name: 'TMPDir' },
+            { name: 'initGithub' },
+            { name: 'uploadToRepo' },
+            { name: 'createRepo' },
+            { name: 'createREADME' },
+            { name: 'createCard' },
+          ];
+          try {
+            const result = await builder.create(link);
+            const description = (
+              (result.schema && result.schema.description) || 
+              (result.openGraph && result.openGraph.ogDescription) || 
+              null
+            );
+            const author = (
+                  result.mercury && 
+                  result.mercury.length > 0 && 
+                  result.mercury[0].author && 
+                  result.mercury[0].author.replace(/[\r\n]+/gm, '').trim()
+                ) || 
+                (result.schema && result.schema.author);
+            const article = {
+              url: link,
+              domain: result.domain,
+              author: author && [author],
+              tags: result.tags,
+              title:
+                (result.schema && result.schema.title) ||
+                (result.mercury && result.mercury.length > 0 && result.mercury[0].title) ||
+                (result.openGraph && result.openGraph.ogTitle),
+              description: description && description.replace(/[\r\n]+/gm, '').trim(),
+              lang:
+                (result.openGraph && result.openGraph.ogLocale) || 
+                result.language && result.language.length>0 && result.language[0].isReliable && result.language[0].language || 
+                'en',
+              published:
+                (result.openGraph && result.openGraph.article && result.openGraph.article.published_time) ||
+                (result.mercury && result.mercury.length > 0 && result.mercury[0].date_published) ||
+                (result.openGraph && result.openGraph.article && result.openGraph.article.modified_time) ||
+                (result.schema && result.schema.modified),
+              updated:
+                (result.openGraph && result.openGraph.article && result.openGraph.article.modified_time) ||
+                (result.mercury && result.mercury.length > 0 && result.mercury[0].date_published) ||
+                (result.schema && result.schema.modified),
+            };
+            console.log(article);
+            const msg = JSON.stringify({
+              name: "CREATE_ARTICLE",
+              payload: article,
+            });
+            await channel.sendToQueue(queue, Buffer.from(msg));
+            console.log(" [x] Sent %s", msg);
+          } catch (error) {
+            console.log(error);
+          }
         }
-      });
+      }
+
     }, {
         noAck: true
     });
